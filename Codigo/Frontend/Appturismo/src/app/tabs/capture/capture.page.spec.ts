b6ffae2cefa54f7b647f3ed468c2b571f5939694
Josefina <finiaramburu1@gmail.com>
2025-10-06 11:54:31 -0300

    modificaciones

--- Codigo/Frontend/Appturismo/src/app/tabs/capture/capture.page.spec.ts
+++ Codigo/Frontend/Appturismo/src/app/tabs/capture/capture.page.spec.ts
@@ -1,11 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 import { CapturePage } from './capture.page';
 
 describe('CapturePage', () => {
   let component: CapturePage;
   let fixture: ComponentFixture<CapturePage>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CapturePage],
+      imports: [
+        CommonModule,   // para *ngIf y *ngFor
+        FormsModule,    // si tu página usa formularios
+        IonicModule.forRoot()  // necesario para componentes <ion-*>
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(CapturePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
