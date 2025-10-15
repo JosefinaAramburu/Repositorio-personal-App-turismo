a667802c9ec97b4fa75924a98ede5a65decfacf2
Josefina <finiaramburu1@gmail.com>
2025-10-15 13:59:41 -0300

    correciones

--- Codigo/Frontend/Appturismo/src/app/tabs/home/home.page.ts
+++ Codigo/Frontend/Appturismo/src/app/tabs/home/home.page.ts
@@ -1,26 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {  IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardTitle, IonCardHeader, IonLabel, IonIcon, IonItem, IonList, IonImg, IonSearchbar } from '@ionic/angular/standalone';
-
+import { 
+  IonContent, 
+  IonHeader, 
+  IonTitle, 
+  IonToolbar, 
+  IonCard, 
+  IonCardContent, 
+  IonCardTitle, 
+  IonCardHeader, 
+  IonLabel, 
+  IonIcon, 
+  IonItem, 
+  IonList, 
+  IonImg, 
+  IonSearchbar 
+} from '@ionic/angular/standalone';
 
 @Component({
-  selector: 'app-quehacer',
-  templateUrl: './quehacer.page.html',
-  styleUrls: ['./quehacer.page.scss'],
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardContent, IonCardTitle, IonCardHeader, IonLabel, IonIcon, IonItem, IonList, IonImg, IonSearchbar]
+  imports: [
+    CommonModule, 
+    FormsModule,
+    IonContent, 
+    IonHeader, 
+    IonTitle, 
+    IonToolbar, 
+    IonCard, 
+    IonCardContent, 
+    IonCardTitle, 
+    IonCardHeader, 
+    IonLabel, 
+    IonIcon, 
+    IonItem, 
+    IonList, 
+    IonImg, 
+    IonSearchbar
+  ]
 })
-export class QuehacerPage implements OnInit {
-
+export class HomePage implements OnInit {
 
   constructor() { }
-
 
   ngOnInit() {
   }
 
-
 }
-
-
