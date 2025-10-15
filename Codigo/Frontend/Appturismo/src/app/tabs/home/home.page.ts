c014944cfed38bfbf7fbcef38dbdc6b16aa6e695
Josefina <finiaramburu1@gmail.com>
2025-10-15 13:46:37 -0300

    home prueba

--- Codigo/Frontend/Appturismo/src/app/tabs/home/home.page.ts
+++ Codigo/Frontend/Appturismo/src/app/tabs/home/home.page.ts
@@ -1,2 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 
-
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, IonicModule],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss']
+})
+export class HomePage {
+  // Componente básico sin funcionalidad compleja
+}
